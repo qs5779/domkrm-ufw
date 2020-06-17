e7a2026fe5a2c03e750e240e73e4b0789a233769
Quien Sabe <qs5779@mail.com>
2020-06-17 13:07:00 -0400

    Do not use template to remove rule.

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -10,22 +10,19 @@ define ufw::rule (
   $exists = "ufw status | grep -qE '# ${name}'"
 
   if $ensure == 'present' {
-
     exec { "ufw-allow-${name}":
       command => "ufw ${name} comment '${name}'",
       unless  => $exists,
       path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
       require => Package['ufw']
     }
-
-  } else {
-
-    exec { "ufw-delete-${name}":
-      command => epp('ufw/delete-rule.epp', { 'comment' => $name } ),
+  }
+  else {
+    exec { "ufw-delete-${name}":,
+      command => "ufw status numbered | tac | sed '/^[\[]/!d' | grep -E '# ${name}' | cut -d ']' -f 1 | tr -d '[\[ ]' | xargs ufw --force delete",
       onlyif  => $exists,
       path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
       require => Package['ufw']
     }
-
   }
 }
